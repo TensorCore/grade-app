5174d780767e16c5d4e920518c24476ea3acf90f
TensorCore <shakerad0109@gmail.com>
2022-10-23 12:05:42 -0700

    Remove unused import

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,7 +4,6 @@ import Head from "next/head";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import Image from "next/image";
 import { useEffect, useState } from "react";
 import { signIn } from "next-auth/react";
 
