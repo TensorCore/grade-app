e2863e19be035b4392a405aec6203a7365ecad4a
TensorCore <shakerad0109@gmail.com>
2022-11-13 15:30:59 -0800

    Admin Init

--- pages/api/admin/newuser.ts
+++ pages/api/admin/newuser.ts
@@ -1,16 +1,36 @@
-// Create new User
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../lib/prismadb";
-import { NextApiRequest, NextApiResponse } from "next";
+import { unstable_getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const data = req.body;
-    const user = await prisma.user.create({
-        data: {
-            name: data.name,
-            username: data.username,
-            role: data.role,
-            password: data.password,
-        },
-    });
-    res.json(user);
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await unstable_getServerSession(req, res, authOptions);
+
+  if (!session) {
+    res.status(401).json({ error: "Unauthorized" });
+    return;
+  }
+
+  const data = req.body;
+
+  //   @ts-ignore  --Can't augment default session type
+  if (session.role != "ADMIN" && session.id != body.studentId) {
+    res.status(401).json({ error: "Unauthorized" });
+    return;
+  }
+
+  const user = await prisma.user.create({
+    data: {
+      name: data.name,
+      username: data.username,
+      role: data.role,
+      password: data.password,
+    },
+  });
+
+  res.status(200).json({ user });
 }
--- pages/dashboard/admin.tsx
+++ pages/dashboard/admin.tsx
@@ -1,88 +1,78 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
-import { signOut } from 'next-auth/react';
-import { unstable_getServerSession } from 'next-auth';
-import { authOptions } from '../api/auth/[...nextauth]';
+import type { NextPage } from "next";
+import Head from "next/head";
+import { unstable_getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]";
+import AdminNavbar from "../../components/navbar/adminNav";
 
 //Dashboard Page for Grade App
 interface Props {
-    Data:{
-        id: String,
-        username: String,
-        role: String,
-        name: String,
-    },
-    DBData: any
+  Data: {
+    id: String;
+    username: String;
+    role: String;
+    name: String;
+  };
+  DBData: any;
 }
-const Dashboard: NextPage<Props> = ({Data}) => {
-    return (
-        <>
-            <Head>
-                <title>Dashboard</title>
-                <meta name="description" content="Grade App" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+const Dashboard: NextPage<Props> = ({ Data }) => {
+  return (
+    <>
+      <Head>
+        <title>Dashboard</title>
+        <meta name="description" content="Grade App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-            <main>
-                <nav className='nav'></nav>
-                <div className="antialiased min-h-screen w-full bg-gradient-to-br from-primary to-secondary">
-                    <div className="flex flex-col items-center justify-center min-h-screen py-2 text-center">
-                        <h1 className="text-6xl font-bold text-primary-content">Dashboard</h1>
-                        <p className="mt-3 text-2xl text-secondary-content">
-                            {'Hello, ' + Data.username}
-                        </p>
-                        <div className="mt-6">
-                            <Link href="/api/auth/signout">
-                                <a className="px-4 py-2 text-lg font-semibold text-primary-content bg-primary rounded-lg hover:bg-primary-focus transition-colors duration-500">
-                                    Sign Out
-                                </a>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
+      <div className="antialiased">
+        <AdminNavbar
+            role={Data.role}
+            name={Data.name}
+            username={Data.username}
+        />
 
-            </main>
+        <main>
 
-            <footer>
-
-            </footer>
-        </>
-    );
+        </main>
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
 
 export async function getServerSideProps(context: any) {
-    const session:any = await unstable_getServerSession(context.req, context.res, authOptions)
-    if (!session) {
-        return {
-            redirect: {
-                destination: '/',
-                permanent: false,
-            },
-        }
-    }
-
-    if(session.role !== 'ADMIN'){
-        return {
-            redirect: {
-                destination: '/dashboard/' + session.role.toLowerCase(),
-                permanent: false,
-            },
-        }
-    }
-
-    const Data = {
-        id: session.id,
-        role: session.role,
-        username: session.username,
-    }
-
+  const session: any = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authOptions
+  );
+  if (!session) {
     return {
-        props: {
-            Data,
-        },
-    }
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  if (session.role !== "ADMIN") {
+    return {
+      redirect: {
+        destination: "/dashboard/" + session.role.toLowerCase(),
+        permanent: false,
+      },
+    };
+  }
+
+  const Data = {
+    id: session.id,
+    role: session.role,
+    username: session.username,
+  };
+
+  return {
+    props: {
+      Data,
+    },
+  };
 }
