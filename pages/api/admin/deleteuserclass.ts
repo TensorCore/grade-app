376e5e6408b987b7efc9f7fcea45e65cec28c9f1
David Yang <dyang57@ucmerced.edu>
2022-11-13 20:54:50 -0800

    Basic api functionality added

---
+++ pages/api/admin/deleteuserclass.ts
@@ -0,0 +1,33 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../lib/prismadb";
+import { unstable_getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await unstable_getServerSession(req, res, authOptions);
+
+  if (!session) {
+    res.status(401).json({ error: "Unauthorized" });
+    return;
+  }
+
+  const data = req.body;
+
+  //   @ts-ignore  --Can't augment default session type
+  if (session.role != "ADMIN" && session.id != body.adminId) {
+    res.status(401).json({ error: "Unauthorized" });
+    return;
+  }
+
+  const userClass = await prisma.userClass.delete({
+    where: {
+        id: data.userClassId,
+    },
+  });
+
+  res.status(200).json({ userClass });
+}
